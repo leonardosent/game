fa79fda2b3c2a1b0177e892c9d2b65d2e69276a6
Leo Sent <50750245+leonardosent@users.noreply.github.com>
2022-12-13 12:26:22 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -9,19 +9,19 @@ addEventListener('resize', () => {
   canvas.height = innerHeight
 })
 
-c.strokeStyle = 'white'
-c.fillStyle = 'blue'
-c.rect(100, 20, 150, 100)
-c.stroke()
-c.fill()
+// c.strokeStyle = 'white'
+// c.fillStyle = 'blue'
+// c.rect(100, 20, 150, 100)
+// c.stroke()
+// c.fill()
 
-c.fillStyle = 'red'
-c.fillRect(400, 500, 300, 250)
+// c.fillStyle = 'red'
+// c.fillRect(400, 500, 300, 250)
 
-// Uncomment to remove the first two blocks
-// c.clearRect(0, 0, canvas.width, canvas.height)
-c.fillStyle = 'green'
-c.fillRect(1500, 500, 300, 250)
+// // Uncomment to remove the first two blocks
+// // c.clearRect(0, 0, canvas.width, canvas.height)
+// c.fillStyle = 'green'
+// c.fillRect(1500, 500, 300, 250)
 
 // Just a basic line
 c.beginPath()
